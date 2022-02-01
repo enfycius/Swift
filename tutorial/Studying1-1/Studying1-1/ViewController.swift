8f6313275e57366f2af61747bfa7207d26ac21c1
김종혁 <hexk0131@gimjonghyeog-ui-MacBookPro.local>
2022-02-01 20:14:57 +0900

    Studying Swift

--- tutorial/Studying1-1/Studying1-1/ViewController.swift
+++ tutorial/Studying1-1/Studying1-1/ViewController.swift
@@ -9,9 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var myButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        myButton.setTitleColor(.red, for: .normal)
+    }
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        print("The button was printed")
     }
 
 
