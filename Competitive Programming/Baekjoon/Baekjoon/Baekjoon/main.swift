33100528d38f13fe6348940688a075fc64fe3451
김종혁 <enfycius@gimjonghyeog-ui-MacBookPro.local>
2023-07-06 06:54:52 +0900

    1000

---
+++ Competitive Programming/Baekjoon/Baekjoon/Baekjoon/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  Baekjoon
+//
+//  Created by 김종혁 on 2023/07/06.
+//
+
+import Foundation
+
+let arr = readLine()!.split(separator: " ").map{Int(String($0))!}
+
+let A = arr[0]
+let B = arr[1]
+
+print(A + B)
+
+
