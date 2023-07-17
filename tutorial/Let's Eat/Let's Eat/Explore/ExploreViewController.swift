d5e21cdf3da89e39a7078f9d0cc9f456008d75c6
김종혁 <enfycius@gimjonghyeog-ui-MacBookPro.local>
2023-07-17 16:16:37 +0900

    Adding a button to the collection view header

---
+++ tutorial/Let's Eat/Let's Eat/Explore/ExploreViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ExploreViewController.swift
+//  Let's Eat
+//
+//  Created by 김종혁 on 2023/07/17.
+//
+
+import UIKit
+
+class ExploreViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let headerView = collectionView.dequeueReusableSupplementaryView(
+            ofKind: kind, withReuseIdentifier: "header", for: indexPath)
+        
+        return headerView
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "exploreCell", for: indexPath)
+        
+        return cell
+    }
+    
+
+    @IBOutlet var collectionView: UICollectionViewCell!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
