7b1e060affeb25caf69026f5438bfb47c8496a63
김종혁 <enfycius@gimjonghyeog-ui-MacBookPro.local>
2023-07-19 03:35:42 +0900

    Declaring the RestaurantListViewController class

---
+++ tutorial/Let's Eat/Let's Eat/Restaurants/RestaurantListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RestaurantListViewController.swift
+//  Let's Eat
+//
+//  Created by 김종혁 on 2023/07/19.
+//
+
+import UIKit
+
+class RestaurantListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
