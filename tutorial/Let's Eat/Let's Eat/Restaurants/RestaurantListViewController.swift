18377b1ea7c8713b07935cfce1a4a133d3f1deaf
김종혁 <enfycius@gimjonghyeog-ui-MacBookPro.local>
2023-07-19 03:56:53 +0900

    Presenting the Restaurant List screen

--- tutorial/Let's Eat/Let's Eat/Restaurants/RestaurantListViewController.swift
+++ tutorial/Let's Eat/Let's Eat/Restaurants/RestaurantListViewController.swift
@@ -7,12 +7,22 @@
 
 import UIKit
 
-class RestaurantListViewController: UIViewController {
-
+class RestaurantListViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    @IBOutlet var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        collectionView.dequeueReusableCell(withReuseIdentifier: "restaurantCell", for: indexPath)
     }
     
 
