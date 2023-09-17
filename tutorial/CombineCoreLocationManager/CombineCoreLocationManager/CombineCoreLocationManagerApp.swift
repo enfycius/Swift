6ed0ec84d3fc9a568e59ac54bf75fa0e6eaf6d32
김종혁 <enfycius@gimjonghyeog-ui-MacBookPro.local>
2023-09-17 13:59:07 +0900

    MVVM Pattern

---
+++ tutorial/CombineCoreLocationManager/CombineCoreLocationManager/CombineCoreLocationManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineCoreLocationManagerApp.swift
+//  CombineCoreLocationManager
+//
+//  Created by 김종혁 on 2023/09/17.
+//
+
+import SwiftUI
+
+@main
+struct CombineCoreLocationManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ tutorial/CombineCoreLocationManager/CombineCoreLocationManager/LocationManager.swift
@@ -0,0 +1,8 @@
+//
+//  LocationManager.swift
+//  CombineCoreLocationManager
+//
+//  Created by 김종혁 on 2023/09/17.
+//
+
+import Foundation
